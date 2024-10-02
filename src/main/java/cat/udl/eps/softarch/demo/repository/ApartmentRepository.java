48b1e61974eb03c6bea8a1684ed07458359325fd
Júlia Nogales <julianolo71@gmail.com>
2024-10-02 18:53:40 +0200

    feat: findCity()

--- src/main/java/cat/udl/eps/softarch/demo/repository/ApartmentRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ApartmentRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 public interface ApartmentRepository extends CrudRepository<Apartment, Long>, PagingAndSortingRepository<Apartment, Long> {
     Optional<Apartment> findById(@Param("id") Long id);
-    List<Apartment> findByCountry(@Param("country" )String country);
-    List<Apartment> findByPostalCode(@Param("postalCode" )String postalCode);
+    List<Apartment> findByCountry(@Param("country") String country);
+    List<Apartment> findByPostalCode(@Param("postalCode") String postalCode);
+    List<Apartment> findByCity(@Param("city") String city);
 }
