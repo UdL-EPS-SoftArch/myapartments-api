198c20fed369685d8e24f826c99abd4e0948d085
Aniol <aniol0012@gmail.com>
2024-10-02 18:23:32 +0200

    feat: add Apartment class base

---
+++ src/main/java/entities/Apartment.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Apartment {
+    private int id;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
