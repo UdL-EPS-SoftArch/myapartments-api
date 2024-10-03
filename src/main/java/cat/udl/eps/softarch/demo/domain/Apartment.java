9765931504f71030d3622e21301a8b8fffecb832
Aniol <aniol0012@gmail.com>
2024-10-03 09:42:15 +0200

    fix: add missing import

--- src/main/java/cat/udl/eps/softarch/demo/domain/Apartment.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Apartment.java
@@ -1,8 +1,13 @@
 package cat.udl.eps.softarch.demo.domain;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.Date;
 
 
 @EqualsAndHashCode(callSuper = true)
