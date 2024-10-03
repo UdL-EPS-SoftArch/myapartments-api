b82fbad3b37261c7fc3f71b07dcf5eac86c72ad2
Júlia Nogales <julianolo71@gmail.com>
2024-10-03 16:34:13 +0200

    feat: example attributes

--- src/main/java/cat/udl/eps/softarch/demo/domain/ApartmentCharacteristics.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/ApartmentCharacteristics.java
@@ -1,5 +1,8 @@
 package cat.udl.eps.softarch.demo.domain;
 
 public class ApartmentCharacteristics extends Apartment {
-        
+        private boolean hasAirConditioner;
+        private float m2;
+        private int nBathroom;
+        private boolean hasElevator;
 }
