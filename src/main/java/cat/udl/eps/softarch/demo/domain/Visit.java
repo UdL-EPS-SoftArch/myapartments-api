c451647ea0f2c4cb64e3f5c94f81185325e7c85c
Abdellah Lamrabat <udlunieps@gmail.com>
2024-10-03 16:24:26 +0200

    feat: add visit entity

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Visit.java
@@ -0,0 +1,26 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.ZonedDateTime;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+public class Visit extends UriEntity<Long>{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private ZonedDateTime when;
+
+}
