50745c058d9cabd72ab19fad6c21e234b40fd8d7
Júlia Nogales <julianolo71@gmail.com>
2024-10-08 16:13:39 +0200

    feat: findById, Name, Phone

--- src/main/java/cat/udl/eps/softarch/demo/repository/OwnerRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/OwnerRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface OwnerRepository extends CrudRepository<Owner, Long>, PagingAndSortingRepository<Owner, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface OwnerRepository extends CrudRepository<Owner, Long>, PagingAndSortingRepository<Owner, Long> {
+    Optional<Owner> findById(@Param("id") Long id);
+    List<Owner> findByName(@Param("name") String name);
+    List<Owner> findByPhone(@Param("phoneNumber") String phoneNumber);
 }
