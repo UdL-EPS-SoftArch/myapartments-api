61de8655a4b5b053046cdc5b1792caac8310ab95
Júlia Nogales <julianolo71@gmail.com>
2024-10-08 16:34:47 +0200

    fix: id param

--- src/main/java/cat/udl/eps/softarch/demo/repository/OwnerRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/OwnerRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long>, PagingAndSortingRepository<Owner, Long> {
-    Optional<Owner> findById(@Param("id") Long id);
+    Optional<Owner> findById(@Param("id") String id);
     List<Owner> findByName(@Param("name") String name);
     List<Owner> findByPhone(@Param("phoneNumber") String phoneNumber);
-    List<Apartment> findApartmentsByUserId(@Param("id") Long id);
+    List<Apartment> findApartmentsByUserId(@Param("id") String id);
 }
