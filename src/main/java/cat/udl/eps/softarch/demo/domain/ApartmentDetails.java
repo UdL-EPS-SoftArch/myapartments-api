4a0a2fe65855577567e3e48845cf744330bb8f41
arnaumarti <arnaums02@gmail.com>
2024-10-09 18:40:38 +0200

    fixing some errors

--- src/main/java/cat/udl/eps/softarch/demo/domain/ApartmentDetails.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/ApartmentDetails.java
@@ -2,16 +2,24 @@ package cat.udl.eps.softarch.demo.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.validation.constraints.Min;
+import lombok.*;
 
 @Entity
 @Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
 
-public class ApartmentDetails extends UriEntity<Long>{
+public class ApartmentDetails extends UriEntity<Long> {
     @Id
     private Long id;
     private float square;
+    @NonNull
+    @Min(1)
     private int numBathrooms;
+    @NonNull
+    @Min(1)
     private int numBedrooms;
     private boolean hasAC;
     private boolean hasElevator;
