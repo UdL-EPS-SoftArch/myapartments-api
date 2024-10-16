8027cf5218e0e51b03b4895753fe02a00a3a941d
Aniol <aniol0012@gmail.com>
2024-10-16 18:34:14 +0200

    fix: add role owner authority

--- src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
+++ src/main/java/cat/udl/eps/softarch/demo/config/WebSecurityConfig.java
@@ -29,12 +29,11 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((auth) -> auth
                         .requestMatchers(HttpMethod.GET, "/identity").authenticated()
                         .requestMatchers(HttpMethod.POST, "/apartments").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/apartments").hasAuthority("ROLE_OWNER")
                         .requestMatchers(HttpMethod.POST, "/apartments/").hasAnyRole("OWNER")
                         .requestMatchers(HttpMethod.POST, "/apartments/*").hasAnyRole("OWNER")
                         .requestMatchers(HttpMethod.POST, "/users").anonymous()
                         .requestMatchers(HttpMethod.POST, "/users/*").denyAll()
-                        .requestMatchers(HttpMethod.POST, "/apartments").hasAnyRole("OWNER")
-                        .requestMatchers(HttpMethod.POST, "/apartments/*").hasAnyRole("OWNER")
                         .requestMatchers(HttpMethod.POST, "/*/*").authenticated()
                         .requestMatchers(HttpMethod.PUT, "/*/*").authenticated()
                         .requestMatchers(HttpMethod.PATCH, "/*/*").authenticated()
