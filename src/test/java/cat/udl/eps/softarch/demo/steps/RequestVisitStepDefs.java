0d417b54386a2bf80cc4b94d2e0fef11668ea509
Abdellah Lamrabat <udlunieps@gmail.com>
2024-10-29 15:58:06 +0100

    fix: add new changes in visitSteps

--- src/test/java/cat/udl/eps/softarch/demo/steps/RequestVisitStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/RequestVisitStepDefs.java
@@ -1,14 +1,15 @@
 package cat.udl.eps.softarch.demo.steps;
 
 import cat.udl.eps.softarch.demo.domain.Advertisement;
+import cat.udl.eps.softarch.demo.domain.AdvertisementStatus;
 import cat.udl.eps.softarch.demo.domain.Visit;
 import cat.udl.eps.softarch.demo.repository.AdvertisementRepository;
-import cat.udl.eps.softarch.demo.repository.UserRepository;
+import cat.udl.eps.softarch.demo.repository.AdvertisementStatusRepository;
 import cat.udl.eps.softarch.demo.repository.VisitRepository;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -30,13 +31,15 @@ public class RequestVisitStepDefs {
 
     @Autowired
     private AdvertisementRepository advertisementRepository;
+    @Autowired
+    private AdvertisementStatusRepository advertisementStatusRepository;
 
     private MvcResult result;
+    private Advertisement advertisement;
 
-    @Given("There is an advertisement with and title {string} and address {string}")
-    public void thereIsAnAdvertisementWithIdAndTitleAndAddress(long advertisementId, String title, String address) {
-        Advertisement advertisement = new Advertisement();
-        advertisement.setId(advertisementId);
+    @Given("There is an advertisement with title {string} and address {string}")
+    public void thereIsAnAdvertisementWithTitleAndAddress(String title, String address) {
+        advertisement = new Advertisement();
         advertisement.setTitle(title);
         advertisement.setAddress(address);
         advertisement.setDescription("A cozy loft in the center of Barcelona");
@@ -44,18 +47,19 @@ public class RequestVisitStepDefs {
         advertisement.setZipCode("08001");
         advertisement.setCountry("Spain");
         advertisement.setCreationDate(ZonedDateTime.now());
+        AdvertisementStatus cur_status = advertisementStatusRepository.findByStatus("Available").stream().findFirst().orElse(null);
+        advertisement.setAdStatus(cur_status);
 
-        advertisementRepository.save(advertisement);
+        advertisement = advertisementRepository.save(advertisement);
     }
 
     @When("I request a visit to the advertisement with title {string}")
     public void iRequestAVisitToTheAdvertisementWithTitle(String title) throws Exception {
+        // Ensure that the advertisement exists
         Advertisement advertisement = advertisementRepository.findByTitle(title).get(0);
-        assertNotNull(advertisement);
+        assertNotNull(advertisement, "Advertisement should exist");
 
-        Visit visit = new Visit();
-        visit.setAdvertisement(advertisement);
-
+        // Request visit using MockMvc to simulate a POST request
         result = mockMvc.perform(post("/visits")
                         .contentType("application/json")
                         .content("{\"advertisement\":{\"id\":" + advertisement.getId() + "}}"))
@@ -65,6 +69,7 @@ public class RequestVisitStepDefs {
 
     @Then("The visit is successfully requested")
     public void theVisitIsSuccessfullyRequested() {
-        assertNotNull(result);
+        assertNotNull(result, "Result should not be null after visit request");
+        Assertions.assertEquals(201, result.getResponse().getStatus(), "Expected HTTP status 201 Created");
     }
 }
