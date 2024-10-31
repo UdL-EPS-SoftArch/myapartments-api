87cf1bbebd6054b1fbab02f6eddcebb01b4eaccb
Júlia Nogales <julianolo71@gmail.com>
2024-10-31 18:25:54 +0100

    feat: description definition

--- src/test/java/cat/udl/eps/softarch/demo/steps/UpdateApartmentStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/UpdateApartmentStepDefs.java
@@ -64,6 +64,32 @@ public class UpdateApartmentStepDefs {
         assertFalse("Apartment with name \"" + name + "\" should exist", apartments.isEmpty());
     }
 
+    @Given("Exists an apartment registered as {string} with description {string}")
+    public void existsAnApartmentRegisteredWithTheDescription(String name, String description) throws Exception {
+        List<Apartment> apartments = apartmentRepository.findByName(name);
+        if (apartments.isEmpty()) {
+            Apartment apartment = new Apartment();
+            apartment.setName(name);
+            apartment.setRegistrationDate(ZonedDateTime.now());
+            apartment.setDescription(description);
+
+            Optional<Owner> ownerOptional = ownerRepository.findById(AuthenticationStepDefs.currentUsername);
+
+            ownerOptional.ifPresent(apartment::setOwner);
+
+            stepDefs.result = stepDefs.mockMvc.perform(
+                            post("/apartments")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(apartment))
+                                    .characterEncoding(StandardCharsets.UTF_8)
+                                    .with(AuthenticationStepDefs.authenticate()))
+                    .andDo(print())
+                    .andExpect(status().isCreated());
+        }
+        apartments = apartmentRepository.findByName(name);
+        assertFalse("Apartment with name \"" + name + "\" should exist", apartments.isEmpty());
+    }
+
     @Transactional
     @When("I update it from name {string} to {string}")
     public void iUpdateTheApartmentsName(String name, String new_name) throws Exception {
@@ -83,6 +109,28 @@ public class UpdateApartmentStepDefs {
                 .andExpect(status().isNoContent());
     }
 
+    @Transactional
+    @When("I update {string} from description {string} to {string}")
+    public void iUpdateTheApartmentsDescription(String name, String description, String new_description) throws Exception {
+        List<Apartment> apartments = apartmentRepository.findByName(name);
+        assertFalse("Apartment with name \"" + name + "\" should exist", apartments.isEmpty());
+
+        Apartment apartment = apartments.get(0);
+
+        assertEquals(apartment.getDescription(), description);
+
+        apartment.setDescription(new_description);
+
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        put("/apartments/" + apartment.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(apartment))
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
     @Then("The apartment {string} name is {string}")
     public void theApartmentHasUpdatedNameTo(String name, String new_name) {
         List<Apartment> oldApartments = apartmentRepository.findByName(name);
@@ -90,5 +138,14 @@ public class UpdateApartmentStepDefs {
 
         List<Apartment> newApartments = apartmentRepository.findByName(new_name);
         assertFalse("Apartment with name \"" + new_name + "\" should exist", newApartments.isEmpty());
+    }
+
+    @Then("The apartment {string} description is {string}")
+    public void theApartmentHasUpdatedDescriptionTo(String name, String new_description) {
+        List<Apartment> apartments = apartmentRepository.findByName(name);
+        assertFalse("Apartment with name \"" + name + "\" should exist", apartments.isEmpty());
+
+        Apartment apartment = apartments.get(0);
+        assertEquals(apartment.getDescription(), new_description);
     }
 }
