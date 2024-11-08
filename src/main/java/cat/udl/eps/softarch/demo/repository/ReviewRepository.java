1ec4c6651a976e002a29d16aa86365ee40b381b1
Adrià Creus Nuñez <adriacreus03@gmail.com>
2024-11-08 19:53:48 +0100

    Changed Review Repository

--- src/main/java/cat/udl/eps/softarch/demo/repository/ReviewRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package cat.udl.eps.softarch.demo.repository;
 
+import cat.udl.eps.softarch.demo.domain.Advertisement;
 import cat.udl.eps.softarch.demo.domain.Review;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Arrays;
+import java.util.List;
 
 public interface ReviewRepository extends CrudRepository<Review, Long>{
+    List<Review> findByTitle(@Param("title") String title);;
 }
