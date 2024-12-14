695f7d00321dd5d219d3da37ab3bf446ea755ff0
MoreTheo <theomorenolomero@gmail.com>
2024-12-14 23:54:51 +0100

    add method

--- src/main/java/cat/udl/eps/softarch/demo/repository/AdvertisementRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/AdvertisementRepository.java
@@ -1,6 +1,7 @@
 package cat.udl.eps.softarch.demo.repository;
 
 import cat.udl.eps.softarch.demo.domain.Advertisement;
+import cat.udl.eps.softarch.demo.domain.Apartment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,5 +10,7 @@ import java.util.List;
 
 public interface AdvertisementRepository extends CrudRepository<Advertisement, Long>, PagingAndSortingRepository<Advertisement, Long> {
     List<Advertisement> findByTitle(@Param("title") String title);
+    List<Advertisement> findByApartment(@Param("apart") Apartment apartment);
+
 
 }
