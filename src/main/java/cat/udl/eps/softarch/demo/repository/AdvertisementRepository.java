89db93f03f808580063bea3b4295abf328ffe20b
MoreTheo <theomorenolomero@gmail.com>
2024-12-15 23:21:15 +0100

    add findByApartment

--- src/main/java/cat/udl/eps/softarch/demo/repository/AdvertisementRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/AdvertisementRepository.java
@@ -2,6 +2,7 @@ package cat.udl.eps.softarch.demo.repository;
 
 import cat.udl.eps.softarch.demo.domain.Advertisement;
 import cat.udl.eps.softarch.demo.domain.Apartment;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,7 @@ import java.util.List;
 
 public interface AdvertisementRepository extends CrudRepository<Advertisement, Long>, PagingAndSortingRepository<Advertisement, Long> {
     List<Advertisement> findByTitle(@Param("title") String title);
-    List<Advertisement> findByApartment(@Param("apart") Apartment apartment);
+    List<Advertisement> findByApartment(@Param("apartment")@NotNull Apartment apartment);
 
 
 }
